0db598a817aaed666fe90d0d395e088d0c74cfa7
Jefh <buenojeferson@outlook.com>
2018-08-22 21:05:32 -0300

    Vinculo do usuario com playlist

--- Unify/Unisinos.Spotify.Dominio/Playlist.cs
+++ Unify/Unisinos.Spotify.Dominio/Playlist.cs
@@ -5,6 +5,7 @@ namespace Unisinos.Spotify.Dominio
     public class Playlist
     {
         public int Id { get; set; }
-        public string Nome { get; set; }        
+        public string Nome { get; set; }     
+        public Usuario Criador { get; set; }   
     }
 }
