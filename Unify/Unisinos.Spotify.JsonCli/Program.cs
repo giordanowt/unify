a10cb0b54eca6becaea0dbe6470828f7ec451bca
Jefh <buenojeferson@outlook.com>
2018-08-23 09:12:45 -0300

    Projeto CLI para gerar JSON

---
+++ Unify/Unisinos.Spotify.JsonCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unisinos.Spotify.JsonCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
