0046a3faf447033369d8e5cc373cd9599b05a17c
Jefh <buenojeferson@outlook.com>
2018-09-12 21:34:49 -0300

    Ajuste no mapeamento

--- Unify/Unisinos.Spotify.Dominio/Usuario.cs
+++ Unify/Unisinos.Spotify.Dominio/Usuario.cs
@@ -5,7 +5,6 @@ namespace Unisinos.Spotify.Dominio
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nome { get; set; } 
-        public List<Playlist> PlaylistsCriadas { get; set; }
+        public string Nome { get; set; }
     }
 }
